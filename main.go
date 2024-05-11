e50076aa8a1ec202d0324e52255649db3fc361c8
Parker Shelton <parkerkshelton@gmail.com>
2024-05-11 12:50:18 -0400

    init commit

---
+++ main.go
@@ -0,0 +1,43 @@
+package apiendpoints
+
+import (
+	"database/sql"
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type Book struct {
+	ID    string `json:"id"`
+	Title string `json:"title"`
+}
+
+func getBooks(c *gin.Context) {
+	db, err := sql.Open("mysql", "admin:password@tcp(parker-database.cfhfkqv5cjrl.us-east-1.rds.amazonaws.com:3306)/book_schema")
+	if err != nil {
+		fmt.Print(err.Error())
+	}
+	defer db.Close()
+
+	query := "SELECT * FROM books"
+
+	queryResult, err := db.Query(query)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	var books []Book
+
+	for queryResult.Next() {
+		var bk Book
+		if err := queryResult.Scan(&bk.ID, &bk.Title); err != nil {
+			fmt.Println(books, err)
+		}
+		books = append(books, bk)
+
+	}
+
+	c.IndentedJSON(http.StatusOK, books)
+}
